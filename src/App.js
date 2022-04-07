87d6e3d0beb3e54d692275d93fc4cac29bdc10e7
younesMask <younesmaskouf@gmail.com>
2022-04-07 10:44:00 +0000

    first commit movie app

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import searchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+//e2ff6fde
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=6bcaaf76";
+const movie1 = {
+  Title: "Amazing Spiderman Syndrome",
+  Year: "2012",
+  imdbID: "tt2586634",
+  Type: "movie",
+  Poster: "N/A",
+};
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("spiderman");
+  }, []);
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+
+        <img src={searchIcon} alt="Search icon" onClick={() => {searchMovies(searchTerm)}} />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies Found !</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
